7ad827a1d0846c5975be291ab91819868f8af738
hoang96 <54611782+hoang96@users.noreply.github.com>
2019-08-28 10:21:27 +0700

    create file demo and validate

---
+++ validate.js
@@ -0,0 +1,33 @@
+function validateForm(){
+            // Bước 1: Lấy giá trị của username và password
+            var username = document.register.username.value;
+            var password = document.register.password.value;
+ 
+            // Bước 2: Kiểm tra dữ liệu hợp lệ hay không
+            if (username == ''){
+              alert('Bạn chưa nhập tên đăng nhập');
+              return false;
+            }else {
+              if(username=="hoangtv123"){
+                alert('Tên đăng nhập đã tồn tại.');
+                return false;
+              }
+              else if (password == ''){
+                alert('Bạn chưa nhập mật khẩu');
+                return false;
+              }
+              else{
+                var regex = /[^\w\s]/gi; 
+                if(password.length <9){
+                 alert('Mật khẩu cần ít nhất 9 kí tự');
+                  return false;
+               }else if(regex.test(password) == false){
+                 alert('Mật khẩu cần bao gồm chữ số chữ cái và ký tự đặc biệt.');
+                 return false;
+               }else{
+                 alert('Dữ liệu hợp lệ, ta có thể chấp nhận submit form');
+                 return true;             
+               }        
+             }
+            }
+}
